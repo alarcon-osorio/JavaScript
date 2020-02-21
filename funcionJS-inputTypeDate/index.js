d8b28fd3298c1deb9279c359a8662a7c209ea5ce
Jeison Alarcon <alarcon_osorio@hotmail.com>
2020-02-21 14:11:38 -0500

    Add files via upload

---
+++ funcionJS-inputTypeDate/index.js
@@ -0,0 +1,3 @@
+//Mostrar place holder en campo type date
+
+<input class="input100" type="text" name="expedition" placeholder="Fecha de Expedición de Documento" onclick="ocultarError();" onfocus="(this.type='date')" onblur="(this.type='text')">
